9ca28e39d843fc125dc576cef120203ac3643fcd
mrrogercampbell <mr@rogercampbell.io>
2024-10-30 03:18:46 -0500

    Fixed homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,9 @@ export default function Home() {
     <div className="relative isolate px-4 pt-10 lg:px-8">
       <div className="mx-auto max-w-2xl py-16 sm:py-32 lg:py-48">
         <div className="text-center flex flex-col items-center">
-          <div className="inline-block overflow-x-auto px-4 max-w-full">
-            <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold tracking-tight text-gray-900 whitespace-nowrap">
-              The A.I.D.E. Framework&trade; Dashboard
-            </h1>
-          </div>
+          <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold tracking-tight text-gray-900 whitespace-nowrap">
+            The A.I.D.E. Framework&trade; Dashboard
+          </h1>
           <p className="mt-4 sm:mt-6 text-base sm:text-lg leading-7 sm:leading-8 text-gray-600 px-4">
             AI Implementation and Development Environment Framework - A structured approach to implementing AI solutions in your organization.
           </p>
