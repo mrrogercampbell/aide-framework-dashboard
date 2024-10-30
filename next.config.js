2183e4e32b0fad4b39e8797fa31093d8e2fc84a2
mrrogercampbell <mr@rogercampbell.io>
2024-10-30 01:52:28 -0500

    Working on deployment errors dealing with edge runtime error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    // If you're not using Edge Runtime everywhere, specify which pages should use it
+    runtime: 'nodejs', // or 'edge' if you want to use Edge Runtime
+}
 
 module.exports = nextConfig
